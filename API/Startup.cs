83c904296287d4dd71c88ceeae5725d742051e52
Sanja Brajkovic <sanjja777@hotmail.com>
2021-09-17 10:58:09 +0100

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,7 +25,7 @@ namespace API
     {
         _config = config;
     }
-        // This method gets called by the runtime. Use this method to add services to the container.
+      
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options =>
@@ -36,27 +36,20 @@ namespace API
             services.AddControllers();
             services.AddCors();
         }
-            /*services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });*/
-        //}
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+           
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
