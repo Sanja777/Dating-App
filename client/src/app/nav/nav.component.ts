0cc061ded7ca62d452d0cc7c856bf99d6826a1c5
Sanja Brajkovic <sanjja777@hotmail.com>
2021-10-06 10:39:14 +0100

    Submitting forms working

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -7,34 +10,29 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  model: any= {}
-  loggedIn=<boolean>(false)
-  
+  model: any = {}
+ 
 
-  constructor(private accountService: AccountService) { }
+
+
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+  
   }
- login() {
-   this.accountService.login(this.model).subscribe(response => {
-     console.log(response);
-     this.loggedIn = true; 
-   }, error => {
-console.log(error);
-   })
- }
 
- logout() {
-   this.accountService.logout();
-   this.loggedIn = false;
- }
 
- getCurrentUser() {
-   this.accountService.currentUser$.subscribe(user => {
-     this.loggedIn = !!user;
-   }, error => {
-     console.log(error);
-   })
- }
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+
+  }
+
 }
