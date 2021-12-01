2f63ef6fb81b15eaba440afd99532ffc6478ea99
Sanja Brajkovic <sanjja777@hotmail.com>
2021-12-01 11:18:54 +0000

    EXPANDING register form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import{HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import {BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
@@ -25,6 +24,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
 
 @NgModule({
   declarations: [
@@ -42,6 +42,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     MemberCardComponent,
     MemberEditComponent,
     PhotoEditorComponent,
+    TextInputComponent,
   ],
   imports: [
     BrowserModule,
@@ -49,6 +50,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
    SharedModule,
    NgxSpinnerModule
   ],
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -8,27 +9,56 @@ import { AccountService } from '../_services/account.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
+
 export class RegisterComponent implements OnInit {
-  
-@Output() cancelRegister = new EventEmitter();
 
+  @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
+  
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService,
+     private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.intializeForm();
+  }
+
+  intializeForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required], 
+      password: ['', [Validators.required, 
+        Validators.minLength(4), Validators.maxLength(8)] ],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+    })
+    }
+  
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.get(matchTo)?.value
+       ? null : { isMatching: true }
+    }
   }
 
   register() {
-    
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
-    }
-    )
+
+    //console.log(this.registerForm.value);
+
+    //this.accountService.register(this.model).subscribe(response => {
+    // console.log(response);
+    //this.cancel();
+    // }, error => {
+    // console.log(error);
+    // this.toastr.error(error.error);
+    //  }
+    //  )
   }
 
   cancel() {
